aa9665fe1092d694acc075e29cd3081e74a6e889
root <nishant.bhorodia@gmail.com>
2016-03-04 18:03:37 +0530

    PG-150: added blank scheduled job

---
+++ api/v3/GiftAid.php
@@ -0,0 +1,10 @@
+<?php
+
+function civicrm_api3_gift_aid_makepastyearsubmissions($params) {
+  // Scheduled job stuff...
+  
+  // select all contributions of eligible financial types
+  // make submissions using setDeclaration ?
+  
+  return civicrm_api3_create_success(1, $params);
+}
