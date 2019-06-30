f1e6388ee3dfb5e0249f22031b43c377cc43c811
Matthew Wire (MJW Consulting) <mjw@mjwconsult.co.uk>
2019-06-30 12:24:43 +0100

    Fix issues with entity definition and regenerate

--- xml/schema/CRM/Civigiftaid/BatchSettings.entityType.php
+++ xml/schema/CRM/Civigiftaid/BatchSettings.entityType.php
@@ -2,7 +2,6 @@
 // This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
 return [
-  0 => 
   [
     'name' => 'BatchSettings',
     'class' => 'CRM_Civigiftaid_DAO_BatchSettings',
